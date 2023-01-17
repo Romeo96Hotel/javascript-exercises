525fb98072068cee3ec0b247f0665869fa806ee5
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-17 16:49:48 -0300

    Lesson 1-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let greeting = string;
+  let number = num;
+  let newGreeting;
 
+  if (num < 0) {
+    newGreeting = "ERROR";
+  } else {
+    newGreeting = greeting.repeat(number);
+  }
+
+  return newGreeting;
 };
+
+const greeting = "hey";
+let number;
+
+console.log(repeatString(greeting, number));
 
 // Do not edit below this line
 module.exports = repeatString;
