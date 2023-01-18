768c7529da123f74142e306291396da09997a4b1
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-18 19:44:59 -0300

    Passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (intA, intB) {
+  let num;
 
+  if (intA === NaN) num = "ERROR";
+
+  if (intA > intB)
+    for (i = 0; i <= intA; i++) {
+      num += i;
+    }
+
+  for (i = 0; i <= intB; i++) {
+    num += i;
+  }
+
+  return num;
 };
 
 // Do not edit below this line
