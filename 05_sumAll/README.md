24292040400768ca85819d3d15d4d2c1177a4139
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-31 14:58:59 -0300

    Made changes to sum all function

--- 05_sumAll/README.md
+++ 05_sumAll/README.md
@@ -3,7 +3,7 @@
 Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
 
 ```javascript
-sumAll(2, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 ```
 
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,17 +3,19 @@ const sumAll = function (intA, intB) {
 
   if (intA === NaN) num = "ERROR";
 
-  if (intA > intB)
+  if (intA > intB) {
     for (i = 0; i <= intA; i++) {
       num += i;
     }
-
-  for (i = 0; i <= intB; i++) {
-    num += i;
+  } else {
+    for (i = 0; i <= intB; i++) {
+      num += i;
+    }
   }
-
   return num;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
