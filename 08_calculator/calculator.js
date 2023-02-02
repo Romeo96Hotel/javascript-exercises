3268917029f75d9e1860f77e2bc693dedff3e116
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-02-01 22:52:49 -0300

    Finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2, ...numbers) {
+  const total = numbers.reduce((a, b) => a + b, 0);
+  return parseInt(num1 + num2 + total);
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2, ...numbers) {
+  const total = numbers.reduce((a, b) => a - b, 0);
+  return parseInt(num1 - num2 - total);
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let total;
+  numbers.length === 0
+    ? (total = 0)
+    : ((total = numbers.reduce((a, b) => a + b)), 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let total;
+  numbers.length === 0
+    ? (total = 0)
+    : ((total = numbers.reduce((a, b) => a * b)), 0);
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let myArray = [];
+  for (let i = 0; i < num; i++) {
+    const indexes = num - i;
+    myArray.push(indexes);
+  }
+  const factorized = myArray.reduce((a, b) => a * b, 1);
+  return factorized;
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
