3bbf52b56a594f3319dca2dd647f9dff975822dd
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-02-01 23:56:40 -0300

    Not sure why 09_palindromes isnt working

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let isPalindrome;
+  const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  const newString = processedString.split().reverse().join();
+  processedString === newString
+    ? (isPalindrome = true)
+    : (isPalindrome = false);
+  return isPalindrome;
 };
 
 // Do not edit below this line
